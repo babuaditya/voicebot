6b287e36922f552600664c7fab15b75d4cc3f082
babuaditya <145614866+babuaditya@users.noreply.github.com>
2024-01-19 14:26:15 +0530

    Update install-asterisk.sh

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -1,5 +1,7 @@
 sudo apt update && sudo apt upgrade
 sudo apt install wget build-essential subversion
+sudo apt-get install -y mpg123 liblwp-protocol-https-perl sox libwww-perl
+sudo apt-get install -y libjson-perl libio-socket-ssl-perl flac
 cd /usr/src/
 sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
 sudo tar zxf asterisk-18-current.tar.gz
@@ -15,8 +17,8 @@ sudo make install
 sudo make samples
 sudo make config
 sudo ldconfig
-
 sudo systemctl start asterisk
+
 sudo asterisk -vvvr
 
 sudo ufw allow 5060/udp
